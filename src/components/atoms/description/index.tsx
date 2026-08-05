7a523c621ff4d54f6677c628b5e8ecdcde362f1c
jsvictoria <juliasantos7652@gmail.com>
2026-08-05 17:02:32 -0300

    feat: new cvs links

--- src/components/atoms/description/index.tsx
+++ src/components/atoms/description/index.tsx
@@ -1,30 +1,32 @@
-import './styles.css';
+import "./styles.css";
 import { useTyping } from "./helpers";
-import { descriptionMessages } from './texts';
-import { useLanguage } from '../../../context/LanguageContext';
-import { Linkedn, Github, Curriculum } from './constants';
+import { descriptionMessages } from "./texts";
+import { useLanguage } from "../../../context/LanguageContext";
+import { Linkedn, Github, Curriculum } from "./constants";
 
 export const Description = () => {
   const { getT, currentLanguage } = useLanguage();
-  const t = getT(descriptionMessages); 
-  const helloText = t('helloText'); 
-  const nameText = t('nameText');   
-  const sitesText = t('sitesText')
+  const t = getT(descriptionMessages);
+  const helloText = t("helloText");
+  const nameText = t("nameText");
+  const sitesText = t("sitesText");
 
-  const cv_pt = 'https://drive.google.com/file/d/1eXMuNaRCaFMqStngs6_xUDPHnntj-jGS/view';
-  const cv_en = 'https://drive.google.com/file/d/1Vdsz0HsYnUhd8iPMau23v1cdMpwTgaz4/view';
+  const cv_pt =
+    "https://drive.google.com/file/d/1pmOJkmnGt2n05tlWuYo5sOvnbOIVot2I/view?usp=drive_link";
+  const cv_en =
+    "https://drive.google.com/file/d/1_4Z5YZDKVJhlUOSoG04QEfFH-LIZbcu6/view?usp=drive_link";
 
-  const resumeLink = currentLanguage === 'pt' ? cv_pt : cv_en;
+  const resumeLink = currentLanguage === "pt" ? cv_pt : cv_en;
 
   const typedHello = useTyping({ text: helloText, startDelay: 800 });
-  const typedName  = useTyping({ text: nameText, startDelay: 500 });
+  const typedName = useTyping({ text: nameText, startDelay: 500 });
   const typedSites = useTyping({ text: sitesText, startDelay: 1000 });
 
-  return(
-    <div className='container-description'>
-      <h2 className='hello typing'>{typedHello}</h2>
-      <h1 className='name typing'>{typedName}</h1>
-      <h3 className='sites typing'>{typedSites}</h3>
+  return (
+    <div className="container-description">
+      <h2 className="hello typing">{typedHello}</h2>
+      <h1 className="name typing">{typedName}</h1>
+      <h3 className="sites typing">{typedSites}</h3>
 
       <ul className="container-icons">
         <li className="icon">
@@ -48,11 +50,7 @@ export const Description = () => {
         </li>
 
         <li className="icon">
-          <a
-            href={resumeLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href={resumeLink} target="_blank" rel="noopener noreferrer">
             <Curriculum />
           </a>
         </li>
